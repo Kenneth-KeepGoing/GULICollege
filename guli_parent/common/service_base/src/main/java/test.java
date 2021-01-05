8e2c8c12deda3bfea5c8a33e573623ad0014dcce
zengyihang <CL30240@163.com>
2021-01-05 18:31:19 +0800

    test

--- guli_parent/common/service_base/src/main/java/test.java
+++ guli_parent/common/service_base/src/main/java/test.java
@@ -4,4 +4,13 @@
  * @Date: 2021-01-05 18:18
  */
 public class test {
+    /**
+     * @Method test
+     * @Date 2021/1/5 0005 18:30
+     * @param
+     * @Return void
+     */
+    public void test(){
+
+    }
 }
